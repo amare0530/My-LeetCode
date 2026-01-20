f171107e4772a6fb7115e98d6546ce94d8418e2f
賴裕升 <bensonlai123tw@gmail.com>
2026-01-20 14:54:39 +0800

    Time: 0 ms (100%), Space: 17.8 MB (9.85%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        string s;
+
+        for(string s : operations){
+             if(s == "++X" || s == "X++"){
+            x += 1;
+            }
+            else{
+                x -= 1;
+            }
+        }
+       
+        return x;
+    }
+};
